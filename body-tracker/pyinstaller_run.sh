ae0835008c5b861262b5fbd347729f83d971c884
Bruno Gonçalves <bigbruno@gmail.com>
2024-10-28 07:53:40 -0300

    one more try

--- body-tracker/pyinstaller_run.sh
+++ body-tracker/pyinstaller_run.sh
@@ -11,7 +11,11 @@ export DISPLAY=:99
 sudo Xvfb -ac :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 sleep 5 
 
-pyinstaller -y --collect-all lib-dynload --collect-data mediapipe big_head_tracker.py
+# pyinstaller -y --collect-all lib-dynload  --collect-all _contextvars --collect-data mediapipe big_head_tracker.py
+
+pyinstaller -y --hidden-import lib-dynload --hidden-import _contextvars --hidden-import contextvars --collect-data mediapipe big_head_tracker.py
+
+
 
 #cp -f "$(readlink -f /usr/lib/x86_64-linux-gnu/libpython*.so)" dist/big_head_tracker/_internal/
 
