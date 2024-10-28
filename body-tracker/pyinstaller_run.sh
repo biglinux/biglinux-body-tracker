d7c18541452d5ca541e7e0732fa029b078994f97
Bruno Gonçalves <bigbruno@gmail.com>
2024-10-27 23:29:22 -0300

    one more try

--- body-tracker/pyinstaller_run.sh
+++ body-tracker/pyinstaller_run.sh
@@ -11,18 +11,18 @@ export DISPLAY=:99
 sudo Xvfb -ac :99 -screen 0 800x600x24 > /dev/null 2>&1 &
 sleep 5 
 
-pyinstaller --collect-data mediapipe big_head_tracker.py
+pyinstaller --collect-all lib-dynload --collect-all contextvars --collect-all _contextvars --collect-data mediapipe big_head_tracker.py
 
 if [[ "$(ls -1 dist/big_head_tracker/_internal/libpython*)" != "" ]]; then
-    pyinstaller --collect-data mediapipe big_head_tracker.py
+    pyinstaller --collect-all lib-dynload --collect-all contextvars --collect-all _contextvars --collect-data mediapipe big_head_tracker.py
 fi
 
 if [[ "$(ls -1 dist/big_head_tracker/_internal/libpython*)" != "" ]]; then
-    pyinstaller --collect-data mediapipe big_head_tracker.py
+    pyinstaller --collect-all lib-dynload --collect-all contextvars --collect-all _contextvars --collect-data mediapipe big_head_tracker.py
 fi
 
 if [[ "$(ls -1 dist/big_head_tracker/_internal/libpython*)" != "" ]]; then
-    pyinstaller --collect-data mediapipe big_head_tracker.py
+    pyinstaller --collect-all lib-dynload --collect-all contextvars --collect-all _contextvars --collect-data mediapipe big_head_tracker.py
 fi
 
 if [[ -e /home/runner/work ]]; then
