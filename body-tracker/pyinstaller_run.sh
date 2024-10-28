9fcbb39cfa6b1f9d864ba9853dc52b8c7a0191af
Bruno Gonçalves <bigbruno@gmail.com>
2024-10-27 22:23:56 -0300

    One more try

--- body-tracker/pyinstaller_run.sh
+++ body-tracker/pyinstaller_run.sh
@@ -9,6 +9,7 @@ pip install Pyinstaller
 
 export DISPLAY=:99
 sudo Xvfb -ac :99 -screen 0 800x600x24 > /dev/null 2>&1 &
+sleep 5 
 
 pyinstaller --collect-all contextvars --collect-all _contextvars --collect-data mediapipe big_head_tracker.py
 
