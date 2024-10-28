5da74dd18d987a24810cc46a22cac925f5a32bdc
Bruno Gonçalves <bigbruno@gmail.com>
2024-10-28 10:07:48 -0300

    one more try

--- body-tracker/pyinstaller_run.sh
+++ body-tracker/pyinstaller_run.sh
@@ -7,20 +7,18 @@ fi
 
 pip install Pyinstaller
 
-export DISPLAY=:99
-sudo Xvfb -ac :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-sleep 5 
+# Only in github actions
+if [[ -e /home/runner/work ]]; then
+    export DISPLAY=:99
+    sudo Xvfb -ac :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+    sleep 5 
+fi
 
-pyinstaller -y --collect-data mediapipe --collect-all lib-dynload big_head_tracker.py
+pyinstaller -y --collect-data mediapipe big_head_tracker.py
 
-# pyinstaller -y --hidden-import lib-dynload --hidden-import _contextvars --hidden-import contextvars --collect-data mediapipe big_head_tracker.py
+#cp -f /usr/lib/x86_64-linux-gnu/libpython*.so dist/big_head_tracker/_internal/
 
-
-
-#cp -f "$(readlink -f /usr/lib/x86_64-linux-gnu/libpython*.so)" dist/big_head_tracker/_internal/
-
-cp -f /usr/lib/x86_64-linux-gnu/libpython*.so dist/big_head_tracker/_internal/
-
+# Only in github actions
 if [[ -e /home/runner/work ]]; then
     rm -R build
     rm -R venv
